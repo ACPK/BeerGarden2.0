5335145ca72974840d41dc53ee0670af3583d446
Tracy Teague <tracy.teague05@gmail.com>
2015-06-04 17:07:24 -0400

    Create view for beers#new.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -11,4 +11,21 @@ class BeersController < ApplicationController
     @comments = @beer.comments
   end
 
+  def new
+    @beer = Beer.new
+  end
+
+  def create
+    beer = Beer.new(beer_params)
+    if beer.save
+      redirect_to '/beers/#{beer.id}'
+    else
+      redirect_to '/beers'
+    end
+  end
+
+  private
+  def beer_params
+    params.require(:beer).permit(:name, :category, :alc_percent, :description).merge(:user_id => @current_user.id)
+  end
 end
