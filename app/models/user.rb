cc0331382fe390f33511b2c9f5e4b2d667bc16da
Tracy Teague <tracy.teague05@gmail.com>
2015-06-04 15:19:35 -0400

    Add validations and constraints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+
+  has_secure_password
+  has_many :beers
+  has_many :comments
+
+  validates :username, :password, :first_name, :last_name, :location, presence: true
+  validates :username,  uniqueness: true
+  validates_length_of :username,
+  :in => 5..15
+  validates_length_of :password,
+  :in => 6..20
 end
