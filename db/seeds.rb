c1bc148b11a0350602a38d37b2610ad211ca9a1e
Tracy Teague <tracy.teague05@gmail.com>
2015-06-04 16:12:25 -0400

    Add faker seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+10.times do
+  User.create(username: Faker::Internet.user_name,
+              password: "123456789",
+              first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              location: Faker::Address.state)
+end
+
+categories = ["IPA", "Stout", "Pale Ale", "Amber", "Wheat Beer", "Cider"]
+
+10.times do
+  Beer.create(name: Faker::Lorem.word,
+              category: categories.sample,
+              alc_percent: Faker::Number.digit,
+              description: Faker::Lorem.sentence(3),
+              user_id: User.all.sample.id)
+end
+
+10.times do
+  Comment.create(content: Faker::Lorem.sentence(3),
+                user_id: User.all.sample.id,
+                beer_id: Beer.all.sample.id)
+end
+
