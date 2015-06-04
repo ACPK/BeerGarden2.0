8b6574e5b79103bdd4a0d627488c8306dce57241
Tracy Teague <tracy.teague05@gmail.com>
2015-06-04 16:37:55 -0400

    Create basic beers#show view.

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -5,4 +5,10 @@ class BeersController < ApplicationController
   def index
     @beers = Beer.all
   end
+
+  def show
+    @beer = Beer.find(params[:id])
+    @comments = @beer.comments
+  end
+
 end
